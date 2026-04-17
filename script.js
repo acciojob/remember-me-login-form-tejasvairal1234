d9b214b9d75c9dc5971dcc144a0b16805723c677
tejasvairal1234 <tejasvairal7066@gmail.com>
2026-04-17 21:52:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const un = document.querySelector("#username");
+const pW = document.querySelector("#password");
+const ck = document.querySelector("#checkbox");
+const sB = document.querySelector("#submit");
+const exB = document.querySelector("#existing");
+
+exB.style.visibility = "hidden";
+
+sB.addEventListener("click", () => {
+	alert(`Logged in as ${un.value}`);
+    this.event.preventDefault();
+});
+
+ck.addEventListener("click", () => {
+    exB.style.visibility = "visible";
+});
+
+exB.addEventListener("click", () => {
+    localStorage.setItem("user", JSON.stringify({usename: `${un.value}`, password: `${pW.value}`}));
+    alert(`Logged in as ${un.value}`);
+    this.event.preventDefault();
+})
+
+
